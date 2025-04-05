6083e70480c3d82d631bd46b46afcf2016263aac
sateeshkovvuri <kovvurisateesh@gmail.com>
2025-04-05 21:15:04 +0530

    timer logic implemented

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 const timer_block = document.getElementById("timer-block");
 timer_block.innerText = "timer will be shown here";
-timer_block.innerText = "timer will be shown here";
 
-setInterval(()=>{},1000);
+const time_elapsed_in_seconds = 0;
+
+setInterval(()=>{
+    time_elapsed_in_seconds++;
+    let hours = Math.floor(time_elapsed_in_seconds/3600);
+    let minutes = Math.floor((time_elapsed_in_seconds%3600)/60);
+    let seconds = Math.floor(time_elapsed_in_seconds%(3600*60));
+    timer_block.innerText = `${hours}hrs: ${minutes}mins: ${seconds}secs`
+},1000);
